5cde650d8ad0e9d8b20229fdada8f2f185fb0d5c
Jazz Mack Smith <j.mack-smith@imperial.ac.uk>
2020-01-31 13:23:46 +0000

    Got file upload and save working

--- papy/papy_dash.py
+++ papy/papy_dash.py
@@ -7,9 +7,12 @@ import pandas as pd
 import numpy as np  
 import base64
 import io
+import os
 import datetime
 import plotly.express as px
 import pa
+
+UPLOAD_DIRECTORY = 'user/'
 
 def generate_table(dataframe, max_rows=5):
     if dataframe is not None:
@@ -24,29 +27,37 @@ def generate_table(dataframe, max_rows=5):
     else:
          return html.Div('Empty dataframe')
 
-def parse_contents(contents, filename, date):
+def save_file(contents, file_name, date):
    
     df = None
     content_type, content_string = contents.split(',')  
     decoded = base64.b64decode(content_string) 
     try:
-        if 'csv' in filename:            
+        if 'csv' in file_name:            
             df = pd.read_csv(io.StringIO(decoded.decode('utf-8')), header=None)
-        elif 'xls' in filename:          
+        elif 'xls' in file_name:          
             df = pd.read_excel(io.BytesIO(decoded))
     except Exception as e:
         return None
-    
-    return df
+    print('saving file', file_name)
+    user_dir_path = os.path.join(UPLOAD_DIRECTORY, str(int(datetime.datetime.now().timestamp())) )
+    if not os.path.exists(user_dir_path):
+        print('making directory', user_dir_path)
+        os.makedirs(user_dir_path)
+
+    new_file = os.path.join(user_dir_path, file_name)
+    print('saving', new_file)
+    df.to_csv(new_file)
+    return new_file
+
 
 app = dash.Dash(__name__)
 #plotly_df = px.data.gapminder().query("country=='Canada'")
-global_df = None
 
 app.layout = html.Div(className='col-sm-12', children=[
     
         html.H3('Power Analysis', className='page-header'),
-     
+        dcc.Store(id='store', storage_type='session'),
         html.Div(className="col-sm-6", children=[
             
             html.Div(className="panel panel-success", children=[
@@ -54,8 +65,7 @@ app.layout = html.Div(className='col-sm-12', children=[
                 html.Div(className="panel-body", children=[
                     html.Div(className="row", children=[
                         html.Div('Upload data:', className='tablelabel col-xs-3'),
-                        html.Div(className='tablevalue col-xs-5', 
-                                 children=dcc.Upload(id='upload-data', children=html.Div(html.Button('Choose CSV file')))),
+                        html.Div(className='tablevalue col-xs-5', children=dcc.Upload(id='upload-data', children=html.Div(html.Button('Choose CSV file')))),
                         html.Div(className='tablelabel col-xs-4', id='output-data-upload')
                     ]),
                     html.Div(className='row', children=[
@@ -70,17 +80,14 @@ app.layout = html.Div(className='col-sm-12', children=[
                             )  
                             ])
                     ]),
-                    html.Div(className='row', children=[
-       
+                    html.Div(className='row', children=[      
                         html.Div(className='tablelabel col-xs-12', children=html.Button('Run analysis', className='btn-primary', id='submit-button', n_clicks=0))
                     ])
                 ])
-            ]),
-            
-            html.Div(id='output-state')
+            ]),          
+            html.Div(id='output-variables')
         ]),
-        html.Div(className="col-sm-6", children=[
-            
+        html.Div(className="col-sm-6", children=[           
             html.Div(className="panel panel-success", children=[
                 html.Div('Default parameters', className="panel-heading"),
                 html.Div(className="panel-body", children=[
@@ -106,40 +113,45 @@ app.layout = html.Div(className='col-sm-12', children=[
                     ])
                 ]),
             ])
-        ]),
-       
+        ]),       
         html.Br(),
         html.Div(id='show-data-table'),
         html.Hr()
 ])
 
-@app.callback(Output('output-state', 'children'),
+@app.callback([Output('output-data-upload', 'children'), Output('store', 'data')],
+              [Input('upload-data', 'contents')],
+              [State('upload-data', 'filename'), State('upload-data', 'last_modified')])
+def load_data_file(contents, file_name, mod_date):
+    children = [ html.Div('No file supplied')]
+    saved_file = None
+    print('loading', file_name)
+    if contents is not None and file_name is not None:
+        #df = parse_contents(contents, file_name, mod_date)    
+        children=[html.Div(file_name)]
+        print("about to save", file_name)
+        saved_file = save_file(contents, file_name, mod_date)
+    return children, saved_file
+
+@app.callback(Output('output-variables', 'children'),
               [Input('submit-button', 'n_clicks')],
               [State('id_var_range', 'value'), 
                State('id_var_samples', 'value'),
                State('id_var_effects', 'value'),
                State('id_var_repeats', 'value'),
                State('id_var_cpus', 'value'),
-               State('id_var_analysis', 'value')
+               State('id_var_analysis', 'value'),
+               State('store', 'data')
                ])
-def run_analysis(n_clicks,range,samples,effects,repeats,cpus,analysis):
-    if global_df is None:
-        return html.Div('no data to process')
-    else:
-        print('running main function')
-        pa.main_ui(global_df, ranges, samples, effects, repeats, analysis, cpus)
-
-@app.callback([Output('output-data-upload', 'children'),Output('show-data-table', 'children')],
-              [Input('upload-data', 'contents')],
-              [State('upload-data', 'filename'), State('upload-data', 'last_modified')])
-def load_data_file(contents, file_name, mod_date):
-    children = [ html.Div('An error occurred opening file')]
+def run_analysis(n_clicks,range,samples,effects,repeats,cpus,analysis,data):
+    print('running main function with data', data)
     df = None
-    if contents is not None:
-        df = parse_contents(contents, file_name, mod_date)  
-        if df is not None:     
-            children=[html.Div(file_name)]
-    return children, generate_table(df)
+    if data is not None:
+        df = pd.read_csv(data)
+    return generate_table(df)
+    #pa.main_ui(data, ranges, samples, effects, repeats, analysis, cpus)
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
